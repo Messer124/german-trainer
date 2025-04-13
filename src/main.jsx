a83a8a1de38726d318014ecda762d909797e094a
Messer124 <messer124@gmail.com>
2025-04-13 16:46:59 +0200

    Views analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { LevelProvider } from "./contexts/LevelContext";
 import { LocaleProvider } from "./contexts/LocaleContext";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
